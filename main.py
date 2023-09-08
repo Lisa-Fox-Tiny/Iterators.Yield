cf07aa3113570687e5e7da0b086bd138f3d4b5dd
Elizabeth Stolbova <LisaTiny@yandex.ru>
2023-09-08 15:50:58 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+class FlatIterator:
+
+    def __init__(self, list_of_list):
+        self.list_of_list = list_of_list
+
+    def __iter__(self):
+        self.list_iterator = iter(self.list_of_list)
+        self.list_2 = []
+        self.cursor = -1
+        return self
+
+    def __next__(self):
+        self.cursor += 1
+        if len(self.list_2) == self.cursor:
+            self.list_2 = None
+            self.cursor = 0
+            if self.list_2 is None:
+                self.list_2 = next(self.list_iterator)
+        return self.list_2[self.cursor]
+
+
+def test_1():
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+    print(list(FlatIterator(list_of_lists_1)))
+
+
+if __name__ == '__main__':
+    test_1()
+
